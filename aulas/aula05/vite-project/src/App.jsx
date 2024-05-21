e09e76a77be81cdf4c53839c940889ae05cd839e
unknown <2122130028@academico.iesb.br>
2024-05-21 10:54:11 -0300

    aulas 5 e 6

--- aulas/aula05/vite-project/src/App.jsx
+++ aulas/aula05/vite-project/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Perfil from './pages/Perfil'
 import Login from './pages/Login'
 import Layout from './components/Layout'
+import Novo from './pages/Novo'
 
 function App() {
   const {user} = useContext(AuthContext);
@@ -15,6 +16,7 @@ function App() {
           <>
             <Route element={<Layout/>}>
               <Route path='/' element={<Home/>}/>
+              <Route path='/novo' element={<Novo/>}/>
               <Route path='/perfil/:id' element={<Perfil/>}/>
             </Route>
           </>
